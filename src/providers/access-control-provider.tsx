b382b4d97f58a19cd17ff6548bc46cb4e4dd1347
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2024-02-21 17:50:40 +0300

    chore: update @refinedev/cli version

--- src/providers/access-control-provider.tsx
+++ src/providers/access-control-provider.tsx
@@ -1,11 +1,11 @@
-import { CanParams, AccessControlProvider } from "@refinedev/core";
+import { AccessControlProvider } from "@refinedev/core";
 
 /**
  * Check out the Access Control Provider documentation for detailed information
  * https://refine.dev/docs/api-reference/core/providers/accessControl-provider
  **/
 export const accessControlProvider: AccessControlProvider = {
-  can: async ({ resource, action, params }: CanParams) => {
+  can: async ({ resource, action, params }) => {
     console.log("can", {
       resource,
       action,
